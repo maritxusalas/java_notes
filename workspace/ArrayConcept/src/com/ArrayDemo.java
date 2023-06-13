7738d728ddff05be9c0449b51d505acf10cda311
Akash <akash300383@gmail.com>
2023-06-13 14:02:23 +0530

    done

---
+++ workspace/ArrayConcept/src/com/ArrayDemo.java
@@ -0,0 +1,16 @@
+package com;
+
+public class ArrayDemo {
+
+	public static void main(String[] args) {
+		int num[];	// array declaration 
+		int num1[]= {10,20,30,40,50};// array declaration with intialization 
+		System.out.println(num1);
+		System.out.println(num1[0]);
+		System.out.println(num1[1]);
+		System.out.println(num1[2]);
+		System.out.println(num1[3]);
+		System.out.println(num1[4]);
+	}
+
+}
