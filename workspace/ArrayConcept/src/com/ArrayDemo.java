ffdaa1b380441644d1c02dae0e53374912a2dd7e
Akash <akash300383@gmail.com>
2023-06-13 14:37:18 +0530

    done

--- workspace/ArrayConcept/src/com/ArrayDemo.java
+++ workspace/ArrayConcept/src/com/ArrayDemo.java
@@ -3,23 +3,31 @@ package com;
 public class ArrayDemo {
 
 	public static void main(String[] args) {
-		int num[];	// array declaration 
-		int num1[]= {10,20,30,40,50};// array declaration with intialization 
-		
-		System.out.println(num1);
-		System.out.println(num1[0]);
-		System.out.println(num1[1]);
-		System.out.println(num1[2]);
-		System.out.println(num1[3]);
-		System.out.println(num1[4]);
-		System.out.println("Size of array "+num1.length);
-		for(int i=1,n=10; i<=n;  i++) {
-			System.out.println("i = "+i);
-		}
-		System.out.println("retrieve value from array using loop");
-		for(int i=0;i<num1.length;i++) {
-			System.out.println("Array value is "+num1[i]);
-		}
+//		int num[];	// array declaration 
+//		int num1[]= {10,20,30,40,50};// array declaration with intialization 
+//		
+//		System.out.println(num1);
+//		System.out.println(num1[0]);
+//		System.out.println(num1[1]);
+//		System.out.println(num1[2]);
+//		System.out.println(num1[3]);
+//		System.out.println(num1[4]);
+//		System.out.println("Size of array "+num1.length);
+//		for(int i=1,n=10; i<=n;  i++) {
+//			System.out.println("i = "+i);
+//		}
+//		System.out.println("retrieve value from array using loop");
+//		for(int i=0;i<num1.length;i++) {
+//			System.out.println("Array value is "+num1[i]);
+//		}
+		int abc[]=new int[10];
+		System.out.println(abc[0]);
+		System.out.println(abc[9]);
+		//System.out.println(abc[10]);
+		System.out.println("Size of array "+abc.length);
 	}
 
 }
+
+
+
