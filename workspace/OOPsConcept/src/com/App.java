928163d0f22116bfb133d439ce83bcf57338cae2
Akash <akash300383@gmail.com>
2023-06-13 17:05:23 +0530

    done

---
+++ workspace/OOPsConcept/src/com/App.java
@@ -0,0 +1,13 @@
+package com;
+
+public class App {
+
+	public static void main(String[] args) {
+	// if we want to call any method or variable part of another class
+		// we need to create the object. 
+	Car innova = new Car();		
+	innova.start();
+	innova.stop();
+	}
+
+}
