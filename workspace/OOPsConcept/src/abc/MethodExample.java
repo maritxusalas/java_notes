e8f5b7390857190c521ef50ddfef108264001118
Akash <akash300383@gmail.com>
2023-06-13 16:10:51 +0530

    done

--- workspace/OOPsConcept/src/abc/MethodExample.java
+++ workspace/OOPsConcept/src/abc/MethodExample.java
@@ -9,13 +9,24 @@ public class MethodExample {
 	// passing parameter and no return value.
 	static void add(int x, int y) {
 		int sum = x+y;
+		//int mul = x/y;
 		System.out.println("Sum of two number is "+sum);
+	}
+	// passing parameter and return value 
+	static int findLargest(int x, int y) {
+		if(x>y) {
+			return x;
+		}else {
+			return y;
+		}
 	}
 	public static void main(String[] args) {
 		display();
 		add(100, 200);
 		add(1,2);
 		add(3,4);
+		int result = findLargest(10, 50);
+		System.out.println(result);
 	}
 	
 }
