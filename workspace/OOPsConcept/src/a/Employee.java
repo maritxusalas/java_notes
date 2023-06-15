b637445833e6f1654f7d7a8cf005d3e69493e72e
Akash <akash300383@gmail.com>
2023-06-15 16:33:09 +0530

    done

--- workspace/OOPsConcept/src/a/Employee.java
+++ workspace/OOPsConcept/src/a/Employee.java
@@ -1,9 +1,9 @@
 package a;
 
 public class Employee {
-	int id;
-	String name;
-	float salary;			// property or instance variable
+	private int id;
+	private String name;
+	private float salary;			// property or instance variable
 	// empty constructor 
 	Employee() {		// it is use to do some initialization. 
 		System.out.println("Object created...");
@@ -15,13 +15,23 @@ public class Employee {
 	Employee(int id1, String name1, float salary1){
 		id  = id1;
 		name=name1;
-		salary=salary1;
+		//salary=salary1;
+		if(salary1<0) {
+			salary=8000;
+		}else {
+			salary = salary1;
+		}
 	}
 	// method passing parameter but no return type. 
 	void setValue(int id1, String name1, float salary1) {	// local variable 
 		id =id1;
 		name=name1;
-		salary=salary1;
+		//salary=salary1;
+		if(salary1<0) {
+			salary=8000;
+		}else {
+			salary = salary1;
+		}
 	}
 	// method no passing parameter and no return type. 
 	void display() {			// behaviour or function or method 
--- workspace/OOPsConcept/src/a/EmployeeTest.java
+++ workspace/OOPsConcept/src/a/EmployeeTest.java
@@ -11,15 +11,16 @@ public class EmployeeTest {
 	Employee emp4  =new Employee();
 	Employee emp5 = new Employee(104, "Mahesh", 20000);
 	
-	emp1.id=100;
-	emp1.name="lex";
-	emp1.salary=12000;
+//	emp1.id=100;			// we can't set value for variable because variable are private 
+//	emp1.name="lex";
+//	emp1.salary=-12000;
+//    
+//	emp2.id=-101;
+//    emp2.name="Steven";
+//    emp2.salary=14000;
     
-	emp2.id=101;
-    emp2.name="Steven";
-    emp2.salary=14000;
-    
-    
+    emp1.setValue(100, "Lex", -12000);
+    emp2.setValue(101, "Steven", 14000);
     emp3.setValue(102, "Lokesh", 15000);
     emp4.setValue(103, "Raju", 18000);
     emp4.setValue(111, "Reeta", 19000);
