1abb9d589d13522b2ebab2111fa8dca54684f600
Akash <akash300383@gmail.com>
2023-06-15 15:02:16 +0530

    done

--- workspace/OOPsConcept/src/a/Employee.java
+++ workspace/OOPsConcept/src/a/Employee.java
@@ -4,12 +4,18 @@ public class Employee {
 	int id;
 	String name;
 	float salary;			// property or instance variable
-	
+	// empty constructor 
 	Employee() {		// it is use to do some initialization. 
 		System.out.println("Object created...");
 		id=123;
 		name="Unknown";
 		salary=5000;
+	}
+	// parameter constructor. 
+	Employee(int id1, String name1, float salary1){
+		id  = id1;
+		name=name1;
+		salary=salary1;
 	}
 	// method passing parameter but no return type. 
 	void setValue(int id1, String name1, float salary1) {	// local variable 
--- workspace/OOPsConcept/src/a/EmployeeTest.java
+++ workspace/OOPsConcept/src/a/EmployeeTest.java
@@ -5,11 +5,12 @@ public class EmployeeTest {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		
 	Employee emp1 = new Employee();
 	Employee emp2 = new Employee();
 	Employee emp3 = new Employee();
 	Employee emp4  =new Employee();
+	Employee emp5 = new Employee(104, "Mahesh", 20000);
+	
 	emp1.id=100;
 	emp1.name="lex";
 	emp1.salary=12000;
@@ -20,12 +21,14 @@ public class EmployeeTest {
     
     
     emp3.setValue(102, "Lokesh", 15000);
-   
+    emp4.setValue(103, "Raju", 18000);
+    emp4.setValue(111, "Reeta", 19000);
+    
 	emp1.display();
 	emp2.display();
 	emp3.display();
 	emp4.display();
-		
+	emp5.display();	
 		
 	}
 
