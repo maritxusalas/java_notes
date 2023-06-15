0d6c29e2a08330cb2fb1ca1b2b2579a4a5e6b3cd
Akash <akash300383@gmail.com>
2023-06-15 16:46:30 +0530

    done

---
+++ workspace/InheritanceConcept/src/com/EmployeeTest.java
@@ -0,0 +1,13 @@
+package com;
+
+public class EmployeeTest {
+
+	public static void main(String[] args) {
+	Employee emp1 = new Employee();
+	emp1.disEmployee();
+	Manager mgr1 = new Manager();
+	mgr1.disManager();
+	mgr1.disEmployee();
+	}
+
+}
---
+++ workspace/InheritanceConcept/src/com/Manager.java
@@ -0,0 +1,8 @@
+package com;
+
+public class Manager extends Employee{
+
+	void disManager() {
+		System.out.println("Manager information");
+	}
+}
