fd28bf9b0b7b173ea007b8d7945fb68e6f63d977
Akash <akash300383@gmail.com>
2023-06-15 17:22:12 +0530

    done

--- workspace/InheritanceConcept/src/com/Manager.java
+++ workspace/InheritanceConcept/src/com/Manager.java
@@ -1,8 +1,13 @@
 package com;
 
 public class Manager extends Employee{
-
+private int numberOfEmp;
+	
+	void setManager(int numberOfEmp) {
+		this.numberOfEmp=numberOfEmp;
+	}
 	void disManager() {
-		System.out.println("Manager information");
+		//System.out.println("Manager information");
+		System.out.println("Number of employee is "+this.numberOfEmp);
 	}
 }
